90bfd22d1122cd237ee968d62c69a9a168741f9c
zhbhun <zhbhun@gmail.com>
2017-01-19 10:32:33 +0800

    add cnode showcase

---
+++ website/cnode/design/build.sh
@@ -0,0 +1,4 @@
+lessc ./less/common/index.less ./css/common.css --source-map
+lessc ./less/base/index.less ./css/base.css --source-map
+lessc ./less/home/index.less ./css/home.css --source-map
+lessc ./less/detail/index.less ./css/detail.css --source-map
