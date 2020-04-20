4a44b6d825579ad37c1946f14b49b8aca5b47c6c
zhbhun <zhbhub@gmail.com>
2020-04-20 14:55:56 +0800

    preact 对比 react 初始大小和性能

---
+++ framework/preact/examples/preact-vs-react/initial/dist/preact/precache-manifest.980b441a7fbac052790192631a2259b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1937764928f2fd32f872e9b74fab55f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1b535d0511bf4b83d31",
+    "url": "/static/css/main.5e3647ae.chunk.css"
+  },
+  {
+    "revision": "c1b535d0511bf4b83d31",
+    "url": "/static/js/main.ede3a1c8.chunk.js"
+  },
+  {
+    "revision": "48d88fbac94ae361b756",
+    "url": "/static/js/runtime-main.d87d8358.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ framework/preact/examples/preact-vs-react/initial/dist/react/precache-manifest.4fa3dd0449665b3ab89763b7ad5ebb2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae23edab707bf0423ef01a3e71f94a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "706bdef4e914be2d6a37",
+    "url": "/static/css/main.5e3647ae.chunk.css"
+  },
+  {
+    "revision": "4cb6a66dfdabefc94a5a",
+    "url": "/static/js/2.f12aaf96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f12aaf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706bdef4e914be2d6a37",
+    "url": "/static/js/main.66b614ef.chunk.js"
+  },
+  {
+    "revision": "48d88fbac94ae361b756",
+    "url": "/static/js/runtime-main.d87d8358.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
