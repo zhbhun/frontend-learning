794b9adca2dae0e1489d7b1d40c42637543dd62e
zhbhun <zhbhub@gmail.com>
2020-09-02 15:23:11 +0800

    umi hello world

---
+++ framework/umi/examples/hello-world/.umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  layout: {},
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+});
