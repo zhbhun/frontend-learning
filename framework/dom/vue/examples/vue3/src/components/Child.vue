adf65a9f3f36645365ef51ac34474e46f385a15e
zhbhun <zhbhun@gmail.com>
2022-03-19 12:09:50 +0800

    docs: vue3 tester

---
+++ framework/dom/vue/examples/vue3/src/components/Child.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onUpdated } from "vue";
+
+defineProps({
+  user: Object,
+});
+
+onUpdated(() => {
+  console.log(">> ", Date.now(), "child updated");
+});
+</script>
+
+<template>
+  <h1>{{ (user && user.name) || "..." }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ framework/dom/vue/examples/vue3/src/components/Parent.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive, onUpdated } from "vue";
+import Child from './Child.vue'
+
+const state = reactive({
+  title: "Parent",
+  user: {
+    name: "1",
+  },
+});
+
+onUpdated(() => {
+  console.log(">> ", Date.now(), "parent updated");
+});
+</script>
+
+<template>
+  <h1>{{ state.title }}</h1>
+  <button @click="state.title = Date.now()">change parent</button>
+  <button @click="state.user.name = Date.now()">change child</button>
+  <Child :user="state.user" />
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
