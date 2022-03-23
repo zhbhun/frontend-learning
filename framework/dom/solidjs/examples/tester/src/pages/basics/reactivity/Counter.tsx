bb0da2ee007026d9c00ce6b39f30969d707b3891
zhbhun <zhbhun@gmail.com>
2022-03-23 14:55:56 +0800

    docs: solidsjs reactivity

--- framework/dom/solidjs/examples/tester/src/pages/basics/reactivity/Counter.tsx
+++ framework/dom/solidjs/examples/tester/src/pages/basics/reactivity/Counter.tsx
@@ -1,37 +1,35 @@
-```js
-import { render } from "solid-js/web";
-import { createMemo, createSignal } from "solid-js";
+import { render } from 'solid-js/web';
+import { createMemo, createSignal } from 'solid-js';
 
 function Counter() {
   const [count, setCount] = createSignal(0);
 
-  const doubleCount = count() * 2
+  const doubleCount = count() * 2;
 
   // 适用于一次性使用，无需复用
-  const trippleCount = () => count() * 3; 
+  const trippleCount = () => count() * 3;
 
   // 最佳，可供多处复用计算结果值
   const quadrupleCount = createMemo(() => {
-      return count() * 4
+    return count() * 4;
   });
 
-  // DOM 节点反复构造，有点浪费性能
+  // DOM 节点反复构造，有点浪费性能，再需要多处复用 DOM 节点时才使用
   const quintupleCount = createMemo(() => {
-    const currentCount = count() * 5 // count 变化会导致整个方法重新执行
-    return <div>{currentCount}</div>
-  })
+    const currentCount = count() * 5;
+    return <div>{currentCount}</div>;
+  });
 
   // 同上，还是存在类似问题
   const sextupleCount1 = () => {
-    const currentCount = count() * 6 // count 变化会导致整个方法重新执行
-    return <div>{currentCount}</div>
-  }
+    const currentCount = count() * 6;
+    return <div>{currentCount}</div>;
+  };
 
   // 最佳，满足复用的同时确保性能
   const sextupleCount2 = () => {
-    return <div>{count() * 6}</div> // count 变化不会重新常见 div 元素，而是直接修改内容
-  }
-
+    return <div>{count() * 6}</div>;
+  };
 
   setInterval(() => setCount(count() + 1), 1000);
 
@@ -48,5 +46,4 @@ function Counter() {
   );
 }
 
-render(() => <Counter />, document.getElementById('app'));
-```
+export default Counter;
--- framework/dom/solidjs/examples/tester/src/pages/basics/reactivity/index.tsx
+++ framework/dom/solidjs/examples/tester/src/pages/basics/reactivity/index.tsx
@@ -1,9 +1,11 @@
 import type { Component } from 'solid-js';
+import Counter from './Counter'
 
 
 const ReactivityTester: Component = () => {
   return (
     <div>
+      <Counter />
     </div>
   );
 };
