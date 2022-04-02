86e3c06929cb471bffcb3109380c40903005bf78
zhbhun <zhbhun@gmail.com>
2022-04-02 10:21:56 +0800

    docs: windicss vue cli demo

---
+++ language/css/framework/windicss/examples/vue-cli-tester/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-5px mt-[0.3px]">1</div>
+  <button class="bg-hex-b2a8bb">11</button>
+  <button class="bg-[hsl(211.7,81.9%,69.6%)]">11</button>
+  <div class="grid-cols-[auto,1fr,30px]">111</div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ language/css/framework/windicss/examples/vue-cli-tester/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pluginOptions: {
+    windicss: {
+      // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
+      preflight: false,
+    },
+  },
+};
