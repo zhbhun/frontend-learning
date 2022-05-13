f9f65d80646504f2c363770cd06ac13ad24e60de
zhbhun <zhbhun@gmail.com>
2022-05-13 09:22:15 +0800

    docs: js 计算器

---
+++ tutorials/Interview/coding/calculator.js
@@ -0,0 +1,15 @@
+class Calculator {
+  constructor(value) {}
+
+  press() {}
+
+  register() {}
+}
+
+class Plus {}
+
+const calculator = new Calculator(0);
+
+calculator.register(new Plus());
+
+calculator.press("plus", 1);
