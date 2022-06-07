cdc6f2158ccd687d72ec03800b21a5fca85beb73
zhbhun <zhbhun@gmail.com>
2022-06-07 17:27:44 +0800

    docs: vite-plugin-ssr-vue

---
+++ framework/dom/vue/examples/vite-plugin-ssr-vue/renderer/usePageContext.ts
@@ -0,0 +1,21 @@
+// Hook `usePageContext()` to make `pageContext` available from any Vue component.
+// See https://vite-plugin-ssr.com/pageContext-anywhere
+
+import { inject } from 'vue'
+import type { App, InjectionKey } from 'vue'
+import { PageContext } from './types'
+
+export { usePageContext }
+export { setPageContext }
+
+const key: InjectionKey<PageContext> = Symbol()
+
+function usePageContext() {
+  const pageContext = inject(key)
+  if (!pageContext) throw new Error("setPageContext() not called in parent")
+  return pageContext
+}
+
+function setPageContext(app: App, pageContext: PageContext) {
+  app.provide(key, pageContext)
+}
