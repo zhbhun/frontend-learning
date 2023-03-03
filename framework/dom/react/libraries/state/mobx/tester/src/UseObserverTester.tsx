94978aa0aad1e2f6bcbfdd2452fc1af92cc58539
zhbhun <zhbhun@gmail.com>
2023-03-03 13:57:00 +0800

    fix: 测试 mobx 重复计算问题

--- framework/dom/react/libraries/state/mobx/tester/src/UseObserverTester.tsx
+++ framework/dom/react/libraries/state/mobx/tester/src/UseObserverTester.tsx
@@ -1,22 +1,41 @@
-import { useLocalObservable, useObserver } from "mobx-react";
+import { computed, reaction } from 'mobx';
+import { useLocalObservable, useObserver } from 'mobx-react';
+import { useEffect } from 'react';
 
 export default () => {
-  const state = useLocalObservable(() => ({
-    user: {
-      age: 0,
-    },
-    increase() {
-      this.user.age += 1;
-    },
-  }));
-  console.log(">> tester", Date.now());
+  const state = useLocalObservable(
+    () => ({
+      user: {
+        age: 0,
+      },
+      increase() {
+        this.user.age += 1;
+      },
+      get xxx() {
+        console.log('>>xxxxxxxxxxx');
+        return 1;
+      },
+    }),
+    {
+      xxx: computed({
+        keepAlive: true,
+      }),
+    }
+  );
+  // useEffect(() => {
+  //   reaction(
+  //     () => state.xxx,
+  //     () => {
+  //       console.log('123');
+  //     }
+  //   );
+  // }, []);
+  console.log('>> tester111', Date.now(), state);
   return (
     <div>
       {useObserver(() => {
-        console.log(">> tester useLocalObservable", Date.now(), state.user);
-        return (
-          <button onClick={() => state.increase()}>{state.user.age}</button>
-        );
+        console.log('>> tester useLocalObservable', Date.now(), state.user);
+        return <button onClick={() => state.increase()}>{state.user.age}</button>;
       })}
     </div>
   );
