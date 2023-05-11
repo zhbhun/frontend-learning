88010079c34facbb137aa02b90d7dc07a86dd0c7
zhbhun <zhbhun@gmail.com>
2023-05-11 19:13:14 +0800

    feat: ionic 集成 ios

--- framework/native/ionic/examples/hello-tab/capacitor.config.ts
+++ framework/native/ionic/examples/hello-tab/capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'hello-tab',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "hello-tab",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
