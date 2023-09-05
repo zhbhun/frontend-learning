40ce6a85c00aed93e70a3dd4e66d6564ddf5e04c
zhbhun <zhbhun@gmail.com>
2023-09-05 08:09:29 +0800

    docs: panda

---
+++ framework/dom/react/tutorials/practice/css-in-js/panda/tester/src/App.tsx
@@ -0,0 +1,11 @@
+import { css } from "../styled-system/css";
+
+function App() {
+  return (
+    <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+      Hello 🐼!
+    </div>
+  );
+}
+
+export default App;
---
+++ framework/dom/react/tutorials/practice/css-in-js/panda/tester/src/main.tsx
@@ -0,0 +1,13 @@
+import { css } from "../styled-system/css";
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+console.log(css({ fontSize: "2xl", fontWeight: "bold" }));
