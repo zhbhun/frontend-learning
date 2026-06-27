2a230461cb1bca10d36a6f80ffed8bdc41490a73
zhbhun <zhbhun@gmail.com>
2026-06-27 14:05:57 +0800

    chore: electrobun tester

---
+++ framework/desktop/electrobun/tester/electrobun.config.ts
@@ -0,0 +1,29 @@
+import type { ElectrobunConfig } from "electrobun";
+
+export default {
+	app: {
+		name: "hello-world",
+		identifier: "helloworld.electrobun.dev",
+		version: "0.0.1",
+	},
+	build: {
+		views: {
+			mainview: {
+				entrypoint: "src/mainview/index.ts",
+			},
+		},
+		copy: {
+			"src/mainview/index.html": "views/mainview/index.html",
+			"src/mainview/index.css": "views/mainview/index.css",
+		},
+		mac: {
+			bundleCEF: false,
+		},
+		linux: {
+			bundleCEF: false,
+		},
+		win: {
+			bundleCEF: false,
+		},
+	},
+} satisfies ElectrobunConfig;
